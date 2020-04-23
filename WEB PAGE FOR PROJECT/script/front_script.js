784d2daa022ac2c20e09d65f74a57add2038f788
Shivani <shivanijadon98@gmail.com>
2020-04-24 02:54:51 +0530

    add cover page & remove previous one

---
+++ WEB PAGE FOR PROJECT/script/front_script.js
@@ -0,0 +1,34 @@
+
+
+function validate(){
+
+    console.log("validating");
+    let user = document.getElementById("inputEmail3");
+    let mail = document.getElementById("inputPassword3");
+
+    if(user.value === "shivanijadon98@gmail.com" &&
+        mail.value === "#comfyhome")
+    {
+        console.log("valid")
+        return true;
+    }
+    else
+    {
+        console.log("invalid")
+        alert("invalid email or password");
+        return false;
+    }    
+}
+
+var btn = document.getElementById("log_btn");
+btn.addEventListener('click', redirect);
+
+function redirect(){
+    
+    var target = document.getElementById("iot_form");
+    if(validate())
+    {
+        target.action = "html/IOT_Lock.html";
+        target.submit();
+    }
+}
