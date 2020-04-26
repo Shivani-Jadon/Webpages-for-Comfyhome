538719216b6e71e3c2173a862e36fc7533ec4de3
arnavdesk <arnavdesk@gmail.com>
2020-04-27 02:20:00 +0530

    add features

--- WEB PAGE FOR PROJECT/script/camera.js
+++ WEB PAGE FOR PROJECT/script/camera.js
@@ -2,120 +2,126 @@ var fileByteArray = [];
 
 //using webcam API
 //configuring webcam and allowing access
-function configure(){
+function configure() {
 	document.getElementById("instruct").style.display = "none";
 	document.getElementById("my_camera").style.display = "flex";
 
-    Webcam.set({
+	Webcam.set({
 		height: '300',
 		width: '400',
-     image_format: 'jpeg',
-     jpeg_quality: 90
-    });
-    Webcam.attach( '#my_camera' );
+		image_format: 'jpeg',
+		jpeg_quality: 90
+	});
+	Webcam.attach('#my_camera');
 }
-  
-  
-   // preload shutter audio clip
-   var shutter = new Audio();
-   shutter.autoplay = false;
-   shutter.src = navigator.userAgent.match(/Firefox/) ? 'shutter.ogg' : '../audio/shutter.mp3';
-  
+
+
+// preload shutter audio clip
+var shutter = new Audio();
+shutter.autoplay = false;
+shutter.src = navigator.userAgent.match(/Firefox/) ? 'shutter.ogg' : '../audio/shutter.mp3';
+
 function take_snapshot() {
-    // play sound effect
-    shutter.play();
+	// play sound effect
+	shutter.play();
 	document.getElementById("image").style.display = "none";
 	document.getElementById("results").style.display = "flex";
 
-    // take snapshot and get image data
-    Webcam.snap( function(data_uri) {
-    // display results in page
-    document.getElementById('results').innerHTML = 
-     '<img id="imageprev" src="'+data_uri+'"/>';
-    } );
-    
-    Webcam.reset();
+	// take snapshot and get image data
+	Webcam.snap(function (data_uri) {
+		// display results in page
+		document.getElementById('results').innerHTML =
+			'<img id="imageprev" src="' + data_uri + '"/>';
+	});
 
-    var byteArray;
-    var base64image = document.getElementById("imageprev").src;
-    console.log(base64image);
-    
-    var uintArray = Base64Binary.decode(base64image);  
-    var byteArray = Base64Binary.decodeArrayBuffer(base64image); 
+	Webcam.reset();
 
-    console.log(uintArray);
+	var byteArray;
+	var base64image = document.getElementById("imageprev").src;
+	console.log(base64image);
+
+	var byteArray = convertDataURIToBinary(base64image);
 	console.log(byteArray);
-	
+
+	var xmlHttp = new XMLHttpRequest();
+	var ipaddr = document.getElementsByName('pythonServerIp')[0].value;
+	xmlHttp.open("POST", "http://" + ipaddr + ":5000/image", true);
+
+
+	xmlHttp.onload = function (e) {
+		if (xmlHttp.readyState === 4) {
+			if (xmlHttp.status === 200) {
+				console.log("HELLO");
+				console.log(xmlHttp.responseText);
+				var result = JSON.parse(xmlHttp.responseText);
+				var name = result.person;
+				console.log(name);
+				unlock_door(name);
+			} else {
+				console.log("NO");
+				console.error(xmlHttp.statusText);
+
+			}
+		}
+	};
+
+
+	xmlHttp.send(byteArray);
+
 	return;
 
 }
 
-//clearing image
-function clearImg(){
-	
-	document.getElementById("results").style.display = "none";
-	document.getElementById("image").style.display = "flex";
-	
-}
 
-// function for converting base_64_image to byte array
-var Base64Binary = {
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-	
-	/* will return a  Uint8Array type */
-	decodeArrayBuffer: function(input) {
-		var bytes = (input.length/4) * 3;
-		var ab = new ArrayBuffer(bytes);
-		this.decode(input, ab);
-		
-		return ab;
-	},
+// function for unlocking door.
 
-	removePaddingChars: function(input){
-		var lkey = this._keyStr.indexOf(input.charAt(input.length - 1));
-		if(lkey == 64){
-			return input.substring(0,input.length - 1);
-		}
-		return input;
-	},
+function unlock_door(name) {
 
-	decode: function (input, arrayBuffer) {
-		//get last chars to see if are valid
-		input = this.removePaddingChars(input);
-		input = this.removePaddingChars(input);
+	var ipaddr = document.getElementsByName('nodeMcuIp')[0].value;
 
-		var bytes = parseInt((input.length / 4) * 3, 10);
-		
-		var uarray;
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
-		var j = 0;
-		
-		if (arrayBuffer)
-			uarray = new Uint8Array(arrayBuffer);
-		else
-			uarray = new Uint8Array(bytes);
-		
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-		
-		for (i=0; i<bytes; i+=3) {	
-			//get the 3 octects in 4 ascii chars
-			enc1 = this._keyStr.indexOf(input.charAt(j++));
-			enc2 = this._keyStr.indexOf(input.charAt(j++));
-			enc3 = this._keyStr.indexOf(input.charAt(j++));
-			enc4 = this._keyStr.indexOf(input.charAt(j++));
-	
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-	
-			uarray[i] = chr1;			
-			if (enc3 != 64) uarray[i+1] = chr2;
-			if (enc4 != 64) uarray[i+2] = chr3;
-		}
-	
-		return uarray;	
+	if (name === "Arnav") {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", "http://" + ipaddr + "/doorunlock", false);
+		xmlHttp.send(null);
+		window.open("../html/IOT_Lock.html", "_self");
+		window.alert("Welcome Arnav")
+	}
+	else {
+		window.alert("wrong user");
 	}
 }
 
+
+//clearing image
+function clearImg() {
+
+	document.getElementById("results").style.display = "none";
+	document.getElementById("image").style.display = "flex";
+
+}
+
+// function for converting base_64_image to byte array
+
+var BASE64_MARKER = ';base64,';
+
+function convertDataURIToBinary(dataURI) {
+	var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+	var base64 = dataURI.substring(base64Index);
+	var raw = window.atob(base64);
+	var rawLength = raw.length;
+	var array = new Int8Array(new ArrayBuffer(rawLength));
+
+	for (i = 0; i < rawLength; i++) {
+		array[i] = raw.charCodeAt(i);
+	}
+	return array;
+}
+
+
+
+// function to expand 
+function expand() {
+	var element = document.getElementById("web_cam");
+	element.style.maxWidth = "100%";
+	element.style.overflow = "visible";
+}
